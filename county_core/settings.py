909146d60264aed5e0aea95d5cd4a138be6cad9d
kiplah <kiplahvictor27@gmail.com>
2025-06-23 15:27:56 +0300

    Fix: Update CORS settings to allow frontend domain

--- county_core/settings.py
+++ county_core/settings.py
@@ -64,7 +64,12 @@ MIDDLEWARE = [
 ]
 
 # CORS settings
-CORS_ALLOW_ALL_ORIGINS = True
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://machakos-ict-frontend.onrender.com",
+]
+
 
 ROOT_URLCONF = 'county_core.urls'
 
